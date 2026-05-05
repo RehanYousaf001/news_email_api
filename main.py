35016308806b48b3c7867c33cd77acc41fb7de2c
Rana Rehan <rehan.yousaf.rajpoot@gmail.com>
2026-05-05 23:01:20 +0500

    Add links, specify language, dynamic query, limit news, subject add

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 # This is a sample Python script.
+from idlelib import query
 
 # Press Ctrl+F5 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import requests
 from send_email import send_email
-
-url = "https://newsapi.org/v2/everything?q=tesla&from=2026-03-28&sortBy=publishedAt&apiKey=c6dbd3341e0b4d52aadb1359e8a30014"
+query_type="tesla"
+url = ("https://newsapi.org/v2/everything?"
+       f"q={query_type}&"
+       "sortBy=publishedAt&"
+       "apiKey=c6dbd3341e0b4d52aadb1359e8a30014&"
+       "language=en")
 
 request = requests.get(url)
-content = request.text
-
-body = ""
-for article in content["articles"]:
+content = request.json()
+print(content)
+body = "Subject: Email Implementation in python \n\n"
+for article in content["articles"][0:20]:
     if article['title'] is not None:
-        body += article['title'] + "\n" + article['description'] + 2*"\n"
-        body = body.encode("utf-8")
+        body = body + article['title'] + "\n" + article['description'] + "\n" + article["url"] + 2*"\n"
 
-send_email(body)
+body = body.encode("utf-8")
+send_email(message=body)
 
--- send_email.py
+++ send_email.py
@@ -5,15 +5,12 @@ def send_email(message):
     host = "smtp.gmail.com"
     port = 465
 
-    username = "app8flask@gmail.com"
-    password = "YOUR_GMAIL_PASSWORD"
+    username = "rehan.yousaf.rajpoot@gmail.com"
+    password = "jlzl pdds vsni isjp"
 
-    receiver = "app8flask@gmail.com"
+    receiver = "ranarehan687@gmail.com"
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
         server.sendmail(username, receiver, message)
-        
-
-send_email("Hello, how are you?")
